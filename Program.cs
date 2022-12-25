22d044986f39967a5b63144895f88f46cf10072a
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-25 16:51:36 +0100

    Certificat crud (OK)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+using portfolio.dal;
+using portfolio.Services.CertificatService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICertificatService,CertificatService>();
+
 
 var app = builder.Build();
 
