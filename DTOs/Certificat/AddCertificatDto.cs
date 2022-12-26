7f14cab5f90f55c2d82ef80a62b2b47e8c4fea9b
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-26 22:47:30 +0100

    Creation of DTOs for Certificat

---
+++ DTOs/Certificat/AddCertificatDto.cs
@@ -0,0 +1,10 @@
+namespace portfolio.DTOs.Certificat
+{
+    public class AddCertificatDto
+    {
+        public string name { get; set; } = "Azure AZ-900";
+        public string description { get; set; } = "Cloud fundamentals";
+        public string image { get; set; } = "link to my image";
+        public string link { get; set; }  = "https://github.comm/MoussaabOukhouya" ;
+    }
+}
---
+++ DTOs/Certificat/GetCetificatDto.cs
@@ -0,0 +1,11 @@
+namespace portfolio.DTOs.Certificat
+{
+    public class GetCertificatDto
+    {
+        public string Id { get; set; }
+        public string name { get; set; } 
+        public string description { get; set; } 
+        public string image { get; set; } 
+        public string link { get; set; }  
+    }
+}
