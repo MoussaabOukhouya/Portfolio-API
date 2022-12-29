ab13d83d006ef936d08d5cdffd6736c0ae0e1818
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-29 23:25:13 +0100

    Initialization of DbContext and definition of DeleteCertificat

--- Services/CertificatService/CertificatService.cs
+++ Services/CertificatService/CertificatService.cs
@@ -1,42 +1,48 @@
 
 using AutoMapper;
+using portfolio.data;
 using portfolio.DTOs.Certificat;
-using portfolio.models
-;
+using portfolio.models;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace portfolio.Services.CertificatService
 {
     public class CertificatService : ICertificatService
     {
-        IMapper _mapper;
-        private static List<Certificat> certificats = new List<Certificat>{
-            new Certificat{Id = 1 , name="test", description="test",image="test",link="test"},
-            new Certificat{Id = 2 , name="momo", description="dodo",image="mimi",link="khoko"}};
+        private readonly IMapper _mapper;
 
-        public CertificatService(IMapper mapper)
+        private readonly MyDbContext _dbContext;
+
+        public CertificatService(IMapper mapper, MyDbContext dbContext)
         {
+            _dbContext = dbContext;
             _mapper = mapper;
         }
 
         public async Task<ServiceResponse<List<GetCertificatDto>>> AddCertificat(AddCertificatDto addCertificatDto)
         {
             var serviceResponse = new ServiceResponse<List<GetCertificatDto>>();
-            certificats.Add(_mapper.Map<Certificat>(addCertificatDto));
-            serviceResponse.data = certificats.Select(c => _mapper.Map<GetCertificatDto>(c)).ToList();
+            var dbCertificats = await _dbContext.certificats.ToListAsync();
+            dbCertificats.Add(_mapper.Map<Certificat>(addCertificatDto));
+            serviceResponse.data = dbCertificats.Select(c => _mapper.Map<GetCertificatDto>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCertificatDto>>> GetAllCertificats()
         {
             var serviceResponse = new ServiceResponse<List<GetCertificatDto>>();
-            serviceResponse.data = certificats.Select(c => _mapper.Map<GetCertificatDto>(c)).ToList();
+            var dbCertificats = await _dbContext.certificats.ToListAsync();
+            serviceResponse.data = dbCertificats.Select(c => _mapper.Map<GetCertificatDto>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetCertificatDto>> GetCertificatById(int Id)
         {
             var serviceResponse = new ServiceResponse<GetCertificatDto>();
-            var certificat = certificats.FirstOrDefault(c => c.Id == Id);
+            var dbCertificats = await _dbContext.certificats.ToListAsync();
+            var certificat = dbCertificats.FirstOrDefault(c => c.Id == Id);
             serviceResponse.data = _mapper.Map<GetCertificatDto>(certificat);
             return serviceResponse;
         }
@@ -44,9 +50,10 @@ namespace portfolio.Services.CertificatService
         public async Task<ServiceResponse<GetCertificatDto>> UpdateCertificat(UpdateCertificatDto updateCertificatDto)
         {
             var serviceResponse = new ServiceResponse<GetCertificatDto>();
+            var dbCertificats = await _dbContext.certificats.ToListAsync();
             try
             {
-                var certificat = certificats.FirstOrDefault(c => c.Id == updateCertificatDto.Id);
+                var certificat = dbCertificats.FirstOrDefault(c => c.Id == updateCertificatDto.Id);
                 if (certificat is null)
                     throw new Exception($"The certificat with the Id: '{updateCertificatDto.Id}' is not found.");
 
@@ -55,6 +62,27 @@ namespace portfolio.Services.CertificatService
                 certificat.image = updateCertificatDto.image;
                 certificat.link = updateCertificatDto.link;
                 serviceResponse.data = _mapper.Map<GetCertificatDto>(certificat);
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.success = false;
+                serviceResponse.message = ex.Message;
+            }
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetCertificatDto>>> DeleteCertificat(int Id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCertificatDto>>();
+            var dbCertificats = await _dbContext.certificats.ToListAsync();
+            try
+            {
+                var certificat = dbCertificats.FirstOrDefault(c => c.Id == Id);
+                if (certificat is null)
+                    throw new Exception($"The certificat with the Id: '{Id}' is not found.");
+
+                dbCertificats.Remove(certificat);
+                serviceResponse.data = dbCertificats.Select(c => _mapper.Map<GetCertificatDto>(c)).ToList();
             }
             catch (Exception ex)
             {
