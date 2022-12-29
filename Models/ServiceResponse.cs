9ac57969259f8b03606d3b22dabd236b5b0b051f
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-29 23:21:29 +0100

    Initiated the values of the general class ServiceResponse

--- Models/ServiceResponse.cs
+++ Models/ServiceResponse.cs
@@ -4,8 +4,8 @@ namespace portfolio.models
     public class ServiceResponse<T>
     {
         public T data {get;set;}
-        public bool success {get;set;}
-        public string message {get;set;} = string.Empty;
+        public bool success {get;set;} = true ;
+        public string message {get;set;} = "Success";
 
     }
 }
