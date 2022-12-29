9d65e7ba5a53b8ef6e6d08782b03e6c6ffee8f4b
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-29 23:26:24 +0100

    Injection of DbContext with the use of SqlServer

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 
-using portfolio.dal;
+using portfolio.data;
 using portfolio.Services.CertificatService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
