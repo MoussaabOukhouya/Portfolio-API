7142229bb1359eaf7b0948a6785837e4a27e4b53
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-31 00:22:48 +0100

    Database migration files (generated by EF)

---
+++ Migrations/20221230225925_initialCreate.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace portfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    lastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    biographie = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    profession = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    resume = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "certificats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_certificats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_certificats_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "experiences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    companyImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    startDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    endDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_experiences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_experiences_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "socialMedias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_socialMedias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_socialMedias_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tools",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tools", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tools_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    startDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    endDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    methodologie = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    githubLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExperienceId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_projects_experiences_ExperienceId",
+                        column: x => x.ExperienceId,
+                        principalTable: "experiences",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    type = table.Column<int>(type: "int", nullable: false),
+                    level = table.Column<int>(type: "int", nullable: false),
+                    certificatId = table.Column<int>(type: "int", nullable: true),
+                    PersonId = table.Column<int>(type: "int", nullable: true),
+                    ProjectId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_skills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_skills_certificats_certificatId",
+                        column: x => x.certificatId,
+                        principalTable: "certificats",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_skills_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_skills_projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "projects",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_certificats_PersonId",
+                table: "certificats",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_experiences_PersonId",
+                table: "experiences",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_projects_ExperienceId",
+                table: "projects",
+                column: "ExperienceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_skills_certificatId",
+                table: "skills",
+                column: "certificatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_skills_PersonId",
+                table: "skills",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_skills_ProjectId",
+                table: "skills",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_socialMedias_PersonId",
+                table: "socialMedias",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tools_PersonId",
+                table: "tools",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "skills");
+
+            migrationBuilder.DropTable(
+                name: "socialMedias");
+
+            migrationBuilder.DropTable(
+                name: "tools");
+
+            migrationBuilder.DropTable(
+                name: "certificats");
+
+            migrationBuilder.DropTable(
+                name: "projects");
+
+            migrationBuilder.DropTable(
+                name: "experiences");
+
+            migrationBuilder.DropTable(
+                name: "persons");
+        }
+    }
+}
