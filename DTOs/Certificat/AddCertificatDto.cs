a802195512f654761533eecf270e169f3e89aa43
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-30 20:56:13 +0100

    Initialization of Certificat attributes with default values

--- DTOs/Certificat/AddCertificatDto.cs
+++ DTOs/Certificat/AddCertificatDto.cs
@@ -2,9 +2,9 @@ namespace portfolio.DTOs.Certificat
 {
     public class AddCertificatDto
     {
-        public string name { get; set; } = "Azure AZ-900";
-        public string description { get; set; } = "Cloud fundamentals";
-        public string image { get; set; } = "link to my image";
-        public string link { get; set; }  = "https://github.comm/MoussaabOukhouya" ;
+        public string name { get; set; } 
+        public string description { get; set; } 
+        public string image { get; set; } 
+        public string link { get; set; }  
     }
 }
--- Models/Certificat.cs
+++ Models/Certificat.cs
@@ -3,10 +3,10 @@ namespace portfolio.models
     public class Certificat
     {
         public int Id { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
-        public string image { get; set; }
-        public string link { get; set; }
+        public string name { get; set; } = "Azure AZ-900";
+        public string description { get; set; } = "Cloud fundamentals";
+        public string image { get; set; } = "link to my image";
+        public string link { get; set; } = "https://github.comm/MoussaabOukhouya" ;
 
     }
 }
