f2dcfd406c0aef651d54e7933773be221bebdfec
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2022-12-30 21:24:54 +0100

    All models added to the DbContext

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -13,6 +13,12 @@ namespace portfolio.data
 
         public DbSet<Certificat> certificats => Set<Certificat>();
         public DbSet<Experience> experiences => Set<Experience>();
+        public DbSet<Person> persons => Set<Person>();
+        public DbSet<Project> projects => Set<Project>();
+        public DbSet<Skill> skills => Set<Skill>();
+        public DbSet<SocialMedia> socialMedias => Set<SocialMedia>();
+        public DbSet<Tool> tools => Set<Tool>();
+
         
     }
 
