5cda1d76ef52f06841c1b8f997a97f20ab276381
Moussaab Oukhouya <OukhouyaMoussaab@gmail.com>
2023-01-06 19:33:51 +0100

    moved ServiceResponse to SharedFolder

---
+++ Shared/ServiceResponse.cs
@@ -0,0 +1,11 @@
+
+namespace portfolio.Shared
+{
+    public class ServiceResponse<T>
+    {
+        public T data {get;set;}
+        public bool success {get;set;} = true ;
+        public string message {get;set;} = "Success";
+
+    }
+}
